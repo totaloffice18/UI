186027cb2a4332803d9b0650bba9a367f76ee877
totaloffice18 <total.office.18@gmail.com>
2024-08-25 16:30:05 +0900

    最初のコミット

---
+++ app/src/main/java/jp/techacademy/kazuya/ui/MainActivity.kt
@@ -0,0 +1,11 @@
+package jp.techacademy.kazuya.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
